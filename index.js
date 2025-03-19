e46484a387e9667931edfb94137439d839a8f795
motiejus1 <motiejus.baliukonis@vka.lt>
2025-03-19 09:58:19 +0200

    *init

---
+++ index.js
@@ -0,0 +1,18 @@
+//todojames testus su Selenium
+
+//6 testus
+//Create to do
+//Edit to do
+//Delete to do
+//Count todos
+//header visible
+//h1 visible
+
+//Selenium - programa/robotas
+//Mocha - testavimo karkasas Selenium
+//Chai - assertion komandu rinkinys(expect, should)
+
+// npm init
+// npm i selenium-webdriver
+//npm i mocha
+//npm i chai
---
+++ test/TestNo1.js
@@ -0,0 +1,6 @@
+//Seleniume
+//1 failas 1 testas
+//Seleniumas visus testus leidzia paraleliai
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const should = require('chai').should();
